3a52fe5a5794365eca2192b8693fd10541d6d2a8
Solver <taufiqq768@gmail.com>
2026-07-09 15:49:10 +0700

    penambahan data migration

---
+++ database/migrations/2026_07_09_083556_create_it_wh_project_documents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('it_wh_project_documents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('it_wh_project_id')->constrained('it_wh_projects')->onDelete('cascade');
+            $table->enum('type', ['PIR', 'Dokumen']);
+            $table->text('description')->nullable();
+            $table->date('document_date')->nullable();
+            $table->string('file_path')->nullable();
+            $table->string('link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('it_wh_project_documents');
+    }
+};
